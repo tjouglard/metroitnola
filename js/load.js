53a0e30f0234f09a1a835769136d8be17af62b73
Tyler <jtouglard@gmail.com>
2016-02-17 18:19:04 -0600

    first commit

---
+++ js/load.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+document.getElementById("footer1").innerHTML =
+	"<footer id='myfooter'>"
+	+ "<a href='index.html'>Home</a> | <a href='tech.html'>Technologies</a> | <a href='services.html'>Services</a> | <a href='contact.html'>Contact Us</a><br/><br/>"
+	+ "<a href='index.html'>&copy; Metro IT Services LLC</a>"
+	+ "</footer>";
+document.getElementById("header1").innerHTML =
+	"<header>"
+	+ "<div id='banner'>"
+	+ "<div id='welcome'>"
+	+ "<h3>Metro IT Services</h3>"
+	+ "<div class='content_imagetext'>"
+	+ "<div class='content_image'>"
+	+ "<img src='images/image2.png' alt='image1'/>"
+	+ "</div>"
+	+ "</div>"
+	+ "</div>"
+	+ "<div id='welcome_slogan'>"
+	+ "<h3>Striving to make your vision a reality</h3>"
+	+ "</div>"
+	+ "<div class='slideshow'>"
+	+ "<ul class='slideshow'>"
+	+ "<li class='show'><img width='940' height='300' src='images/datacenter.jpg' alt='1' /></li>"
+	+ "<li><img width='940' height='300' src='images/collaboration.jpg' alt='2' /></li>"
+	+ "<li><img width='940' height='300' src='images/serviceprovider.jpg' alt='3' /></li>"
+	+ "<li><img width='940' height='300' src='images/virtualization.jpg' alt='4' /></li>"
+	+ "<li><img width='940' height='300' src='images/wifi.jpg' alt='5' ></li>"
+	+ "<li><img width='940' height='300' src='images/en.jpg' alt='6' /></li>"
+	+ "</div>"
+	+ "</div>"
+	+ "</header>";
+});
---
+++ php/email_form.php
@@ -0,0 +1,19 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="tjouglard@gmail.com,cjouglard@gmail.com,cjouglard@metroitnola.com";
+    $subject="Metro IT NOLA Contact Page Submission";
+	$name=$_POST["name"];
+    $company=$_POST["company"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $name\nCompany: $company\nEmail: $email\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $email <$email>");
+
+
+header ('Location: ../../thankyou.html');
+exit;
+}
+?>
